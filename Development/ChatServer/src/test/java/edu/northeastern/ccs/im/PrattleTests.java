fc447c4299ee3b2a1f9d37386e4502d7c6bc81da
sibendudey <sibendu.dey@gmail.com>
2019-02-19 22:28:17 -0500

    Prattle initial checkin with Junit test suite

---
+++ Development/ChatServer/src/test/java/edu/northeastern/ccs/im/PrattleTests.java
@@ -0,0 +1,18 @@
+package edu.northeastern.ccs.im;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Junit test suite for prattle test classes.
+ *
+ * @author sibendudey
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        // Add the junit classes under here provided by the
+        // fully qualified name of the classes
+})
+public class PrattleTests {
+
+}
