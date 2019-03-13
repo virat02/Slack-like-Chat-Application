88d096725a43cad94746fcdf7e349a53a26133e3
sibendudey <sibendu.dey@gmail.com>
2019-03-13 12:37:50 -0400

    Broadcast service isClientActive method added

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/service/MessageBroadCastService.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/service/MessageBroadCastService.java
@@ -1,5 +1,6 @@
 package edu.northeastern.ccs.im.service;
 
+import edu.northeastern.ccs.im.ChatLogger;
 import edu.northeastern.ccs.im.Message;
 import edu.northeastern.ccs.im.NetworkConnection;
 import edu.northeastern.ccs.im.server.ClientRunnable;
@@ -7,18 +8,17 @@ import edu.northeastern.ccs.im.server.ServerConstants;
 
 import java.io.IOException;
 import java.nio.channels.SocketChannel;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.*;
 
 public class MessageBroadCastService implements BroadCastService {
     private static ConcurrentLinkedQueue<ClientRunnable> active = new ConcurrentLinkedQueue<>();
-    private ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(20);
+    private ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(10);
+    private MessageService messageService = new MessageService();
 
     @Override
     public void addConnection(SocketChannel socketChannel) throws IOException {
         socketChannel.configureBlocking(false);
-        ClientRunnable clientRunnable = new ClientRunnable(new NetworkConnection(socketChannel));
+        ClientRunnable clientRunnable = new ClientRunnable(new NetworkConnection(socketChannel), this);
         active.add(clientRunnable);
         // Have the client executed by our pool of threads.
         ScheduledFuture<?> clientFuture = threadPool.scheduleAtFixedRate(clientRunnable, ServerConstants.CLIENT_CHECK_DELAY,
@@ -26,13 +26,30 @@ public class MessageBroadCastService implements BroadCastService {
         clientRunnable.setFuture(clientFuture);
     }
 
-    public static void broadcastMessage(Message message) {
+    @Override
+    public boolean isClientActive() {
+        return !active.isEmpty();
+    }
+
+    @Override
+    public void broadcastMessage(Message message) {
+        // TODO Handle exception, if message is not created, don't broadcast
+//        if (message.isBroadcastMessage())   {
+//            messageService.createMessage(message.getText(), message.getName(), message.groupCode());
+//        }
         // Loop through all of our active threads
         for (ClientRunnable tt : active) {
             // Do not send the message if it's not ready to be send
             if (tt.isInitialized()) {
                 tt.enqueueMessage(message);
             }
+        }
+    }
+
+    @Override
+    public void removeClient(ClientRunnable dead)  {
+        if (!active.remove(dead)) {
+            ChatLogger.info("Could not find a thread that I tried to remove!\n");
         }
     }
 }
--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/service/MessageService.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/service/MessageService.java
@@ -1,29 +1,21 @@
 package edu.northeastern.ccs.im.service;
 
 import edu.northeastern.ccs.im.service.JPAService.MessageJPAService;
+import edu.northeastern.ccs.im.userGroup.Group;
 import edu.northeastern.ccs.im.userGroup.Message;
+import edu.northeastern.ccs.im.userGroup.User;
 
 
 public class MessageService implements IService{
 
     private MessageJPAService messageJPAService;
+    private UserService userService;
+    private GroupService groupService;
     public MessageService() {
+        messageJPAService = new MessageJPAService();
+        userService = new UserService();
+        groupService = new GroupService();
     }
-
-//    /**
-//     * Create a message object
-//     * @param message
-//     * @param timestamp
-//     */
-//    public Message createMessage(String message, Date timestamp, int expiration) {
-//        if(message.length() != 0) {
-//            Message message1 = new Message(m.getId(), message, timestamp, expiration);
-//            //messageJPAService.createMessage(message1);
-//            return message1;
-//        }
-//        else {
-//            throw new NullPointerException();
-//        }
 
     /**
      * Create and send a message
@@ -31,27 +23,20 @@ public class MessageService implements IService{
      * @return
      */
     public Message createMessage(Message message) {
-
         messageJPAService.createMessage(message);
         return messageJPAService.getMessage(message.getId());
 
     }
 
-//    /**
-//     * Sends a message to a receiver
-//     * @param msg
-//     * @param sender
-//     * @param receiver
-//     * @param deleted
-//     * @return
-//     */
-//    public Message sendMessage(Message msg, UserGroup sender, Group receiver, Boolean deleted) {
-//
-//        this.m = msg;
-//        Message message2 = new Message(msg.getId(), msg.getMessage(), msg.getTimestamp(), msg.getExpiration(), sender, receiver, deleted);
-//        //messageJPAService.createMessage(message2);
-//        return message2;
-//    }
+    public Message createMessage(String messageBody, String userName, String groupCode) {
+        Message message = new Message();
+        User user = userService.search(userName);
+        Group group = groupService.searchUsingCode(groupCode);
+        message.setMessage(messageBody);
+        message.setSender(user);
+        message.setReceiver(group);
+        return createMessage(message);
+    }
 
     /**
      * Get the message
@@ -61,22 +46,6 @@ public class MessageService implements IService{
     public Message get(int id) {
         return messageJPAService.getMessage(id);
     }
-
-    /**
-     * Updates the message
-     * @param msg
-     */
-//    public void updateMessage(Message msg) {
-//        m.setMessage(msg.getMessage());
-//        m.setExpiration(msg.getExpiration());
-//        m.setTimestamp(msg.getTimestamp());
-//        m.setSender(msg.getSender());
-//        m.setReceiver(msg.getReceiver());
-//        m.setDeleted(msg.isDeleted());
-//
-//        //messageJPAService.updateMessage(msg);
-//
-//    }
 
     public Message updateMessage(Message msg) {
         messageJPAService.updateMessage(msg);
