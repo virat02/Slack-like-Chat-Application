0db89fe46cc777200e8d34beada39785c21cd790
sibendudey <sibendu.dey@gmail.com>
2019-04-09 15:40:50 -0400

    Refactor entity manager util

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/service/EntityManagerUtil.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/service/EntityManagerUtil.java
@@ -1,5 +1,6 @@
 package edu.northeastern.ccs.im.service;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -9,21 +10,22 @@ import javax.persistence.Persistence;
  * application to create thread local entitymanager instances.
  */
 public class EntityManagerUtil {
-    private static EntityManagerFactory entityManagerFactory;
-    static {
-        entityManagerFactory = Persistence.createEntityManagerFactory("PrattlePersistance");
-    }
-
-    /**
-     * Enforce singleton pattern, private constructor
-     */
-    private EntityManagerUtil()    {}
+    private EntityManagerFactory entityManagerFactory;
 
     /***
-     * Returns the entitymanagerfactory
-     * @return an instance of EntityManagerFactory.
+     * Creates an instance of this.
      */
-    public static EntityManagerFactory getEntityManagerFactory()    {
-        return entityManagerFactory;
+    public EntityManagerUtil()  {
+        entityManagerFactory = Persistence.createEntityManagerFactory("PrattlePersistance");
+    }
+    /***
+     * Returns a single instance of EntityManager.
+     * EntityManager instances are not thread-safe,
+     * hence it is advisable to use the entitymanager in a thread local
+     * environment instead of being used as an instance.
+     * @return an instance of EntityManager.
+     */
+    public EntityManager getEntityManager()    {
+        return entityManagerFactory.createEntityManager();
     }
 }
--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/service/jpa_service/UserJPAService.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/service/jpa_service/UserJPAService.java
@@ -24,10 +24,11 @@ public class UserJPAService {
     //The entity manager for this class.
     private EntityManager entityManager;
 
+    private EntityManagerUtil entityManagerUtil;
 
-    private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PrattlePersistance");
-
-    public UserJPAService() {}
+    public void setEntityManagerUtil(EntityManagerUtil entityManagerUtil) {
+        this.entityManagerUtil = entityManagerUtil;
+    }
 
     /**
      * The Bill Pugh Singleton Implementation. A private class meant to serve as our instance of the
@@ -246,7 +247,7 @@ public class UserJPAService {
      * from the chatroom.
      */
     public void saveOrUpdateJoinGroupEvent(UserChatRoomLogOffEvent userChatRoomLogOffEvent)  {
-        EntityManager em = EntityManagerUtil.getEntityManagerFactory().createEntityManager();
+        EntityManager em = entityManagerUtil.getEntityManager();
         em.getTransaction().begin();
         em.merge(userChatRoomLogOffEvent);
         em.getTransaction().commit();
@@ -254,7 +255,7 @@ public class UserJPAService {
     }
 
     public UserChatRoomLogOffEvent getLogOffEvent(int userId, int groupId) throws FirstTimeUserLoggedInException {
-        EntityManager em = EntityManagerUtil.getEntityManagerFactory().createEntityManager();
+        EntityManager em = entityManagerUtil.getEntityManager();
         em.getTransaction().begin();
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<UserChatRoomLogOffEvent> cq = cb.createQuery(UserChatRoomLogOffEvent.class);
