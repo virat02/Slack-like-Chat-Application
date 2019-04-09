d0acc7fed66f14f25110735409135012d173970e
sangeethac <chandrashekar.s@husky.neu.edu>
2019-04-09 16:31:10 -0400

    fixing sonar error

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/user_group/converter/CryptoConverter.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/user_group/converter/CryptoConverter.java
@@ -1,6 +1,5 @@
 package edu.northeastern.ccs.im.user_group.converter;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
-
+import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
@@ -31,7 +30,7 @@ public class CryptoConverter implements AttributeConverter<String, String> {
         try {
             Cipher c = Cipher.getInstance(ALGORITHM);
             c.init(Cipher.ENCRYPT_MODE, key);
-            return Base64.encode(c.doFinal(password.getBytes()));
+            return Base64.getEncoder().encodeToString(c.doFinal(password.getBytes()));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -49,7 +48,7 @@ public class CryptoConverter implements AttributeConverter<String, String> {
         try {
             Cipher c = Cipher.getInstance(ALGORITHM);
             c.init(Cipher.DECRYPT_MODE, key);
-            return new String(c.doFinal(Base64.decode(dbData)));
+            return new String(c.doFinal(Base64.getDecoder().decode(dbData)));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
