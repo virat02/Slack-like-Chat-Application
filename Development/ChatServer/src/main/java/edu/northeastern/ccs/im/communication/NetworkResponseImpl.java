423be00d91299f216acd983914174cb875c3133b
Jerry Lanning <lanning.j@husky.neu.edu>
2019-04-12 14:47:14 -0400

    Network Response IMPL JavaDoc

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/communication/NetworkResponseImpl.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/communication/NetworkResponseImpl.java
@@ -9,15 +9,30 @@ public class NetworkResponseImpl implements NetworkResponse {
      */
     public NetworkResponseImpl()    {
     }
+
+    /**
+     * A constructor that takes a status and a payload and creates a Network Response.
+     * @param status the status of the Network Response.
+     * @param payload the Payload of the Network Response.
+     */
     public NetworkResponseImpl(STATUS status, Payload payload)   {
         this.status = status;
         this.payload = payload;
     }
+
+    /**
+     * The status of a Network Response.
+     * @return STATUS of the Network Response.
+     */
     @Override
     public STATUS status() {
         return status;
     }
 
+    /**
+     * The Payload being loaded onto the Network Response.
+     * @return The Payload being added.
+     */
     @Override
     public Payload payload() {
         return payload;
