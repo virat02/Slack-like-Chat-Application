1ac62bb7d5c3df4cf20add423ebb686ba6d2d5b3
Jerry Lanning <lanning.j@husky.neu.edu>
2019-04-12 14:43:16 -0400

    IController javadoc

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/controller/IController.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/controller/IController.java
@@ -7,12 +7,36 @@ import edu.northeastern.ccs.im.communication.NetworkResponse;
  */
 public interface IController<T>{
 
+    /**
+     * A method for adding an entity.
+     * @param entity the entity to be added.
+     * @return a Network response with either pass or fail depending if the
+     * entity was added.
+     */
     NetworkResponse addEntity(T entity);
 
+    /**
+     * A method for search for an entity.
+     * @param username of the entity to be searched.
+     * @return a Network response with either pass or fail depending if the
+     * entity was found.
+     */
     NetworkResponse searchEntity(String username);
 
+    /**
+     * A method for updating an entity.
+     * @param entity the entity to be updated.
+     * @return a Network response with either pass or fail depending if the
+     * entity was updated.
+     */
     NetworkResponse updateEntity(T entity);
 
+    /**
+     * A method for deleting an entity.
+     * @param entity the entity to be deleted.
+     * @return a Network response with either pass or fail depending if the
+     * entity was deleted.
+     */
     NetworkResponse deleteEntity(T entity);
 
 }
