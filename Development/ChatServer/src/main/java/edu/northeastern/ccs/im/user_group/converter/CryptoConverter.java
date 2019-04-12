9d41e5ae0ecf098c5e2deec924683526f07eb763
sangeethac <chandrashekar.s@husky.neu.edu>
2019-04-12 00:29:46 -0400

    updating encryption algorithm

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/user_group/converter/CryptoConverter.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/user_group/converter/CryptoConverter.java
@@ -14,7 +14,7 @@ import java.security.Key;
 @Converter
 public class CryptoConverter implements AttributeConverter<String, String> {
     // Algorithm used by the Cipher
-    private static final String ALGORITHM = "AES/GCM/NoPadding";
+    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
     //Unique Key that is used in Converting the given attribute
     private static final byte[] KEY = "CS5500ProjectSecurityKey".getBytes();
 
