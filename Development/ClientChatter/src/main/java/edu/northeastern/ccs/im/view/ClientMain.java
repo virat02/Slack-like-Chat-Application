36ff190887085846a8e1be7ec8cb716e4a76950e
Tarun Kumar <attayampattysekar.t@husky.neu.edu>
2019-04-12 17:03:49 -0400

    Updating client jar

--- Development/ClientChatter/src/main/java/edu/northeastern/ccs/im/view/ClientMain.java
+++ Development/ClientChatter/src/main/java/edu/northeastern/ccs/im/view/ClientMain.java
@@ -8,24 +8,24 @@ public abstract class ClientMain {
   private static TerminalWindow rootWindow;
 
   public static void main(String[] args) {
-//    if (args.length != 2) {
-//      System.out.println("Invalid Arguments");
-//      System.exit(0);
-//    }
-//    try {
-//      Integer.parseInt(args[1]);
-//    }
-//    catch (NumberFormatException exception) {
-//      System.out.println("Invalid Arguments");
-//      System.exit(0);
-//    }
+    if (args.length != 2) {
+      System.out.println("Invalid Arguments");
+      System.exit(0);
+    }
+    try {
+      Integer.parseInt(args[1]);
+    }
+    catch (NumberFormatException exception) {
+      System.out.println("Invalid Arguments");
+      System.exit(0);
+    }
 
 
     try {
-//      NetworkConstants.setHostName(args[0]);
-//      NetworkConstants.setPortNumber(Integer.parseInt(args[1]));
-      NetworkConstants.setHostName("localhost");
-      NetworkConstants.setPortNumber(4545);
+      NetworkConstants.setHostName(args[0]);
+      NetworkConstants.setPortNumber(Integer.parseInt(args[1]));
+//      NetworkConstants.setHostName("localhost");
+//      NetworkConstants.setPortNumber(4545);
       rootWindow = new RootWindow(null, new ClientConnectionFactory());
       rootWindow.runWindow();
     }
